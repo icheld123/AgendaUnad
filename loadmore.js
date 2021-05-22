349332ea418b7f37a61fd20da5058aa916ca8195
icheld123 <67239021+icheld123@users.noreply.github.com>
2021-05-22 15:47:14 -0500

    Update loadmore.js

--- loadmore.js
+++ loadmore.js
@@ -1,6 +1,31 @@
-jQuery(function(){
-    jQuery('.showSingle').click(function(){
-        jQuery('.targetDiv').hide();
-        jQuery('.div'+jQuery(this).attr('target')).show();
-    });
+var page = 1;
+
+function showPage(page) {
+    $('.pagination .post:not(#page'+page+')').hide();
+    $('.pagination .post#page'+page).show();
+}
+
+function prevPage() {
+    if (page == 1) {
+        page = $('.pagination .post').length;
+    } else {
+        page--;
+    }
+    showPage(page);
+}
+
+function nextPage() {
+    if (page == $('.pagination .post').length) {
+        page = 1;
+    } else {
+        page++;
+    }
+    showPage(page);
+}
+
+$(document).ready(function() {
+    showPage(page);
+    
+    $('#prev').click(prevPage);
+    $('#next').click(nextPage);
 });
